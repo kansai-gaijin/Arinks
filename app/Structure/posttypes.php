38375366af44c6fef11bfc1a4b6b05613fa4d357
kansai-gaijin <windecker.lc@gmail.com>
2020-02-14 00:14:51 +0900

    edited post types

--- app/Structure/posttypes.php
+++ app/Structure/posttypes.php
@@ -16,32 +16,17 @@ namespace Arinks\Theme\App\Structure;
 use function Arinks\Theme\App\config;
 
 /**
- * Registers `book` custom post type.
+ * Registers `teacher` custom post type.
  *
  * @return void
  */
-/* function register_book_post_type()
+/*function register_teacher_post_type()
 {
-    register_post_type('book', [
-        'description' => __('Collection of books.', config('textdomain')),
-        'public' => true,
-        'supports' => ['title', 'editor', 'excerpt', 'thumbnail'],
-        'labels' => [
-            'name' => _x('Books', 'post type general name', config('textdomain')),
-            'singular_name' => _x('Book', 'post type singular name', config('textdomain')),
-            'menu_name' => _x('Books', 'admin menu', config('textdomain')),
-            'name_admin_bar' => _x('Book', 'add new on admin bar', config('textdomain')),
-            'add_new' => _x('Add New', 'book', config('textdomain')),
-            'add_new_item' => __('Add New Book', config('textdomain')),
-            'new_item' => __('New Book', config('textdomain')),
-            'edit_item' => __('Edit Book', config('textdomain')),
-            'view_item' => __('View Book', config('textdomain')),
-            'all_items' => __('All Books', config('textdomain')),
-            'search_items' => __('Search Books', config('textdomain')),
-            'parent_item_colon' => __('Parent Books:', config('textdomain')),
-            'not_found' => __('No books found.', config('textdomain')),
-            'not_found_in_trash' => __('No books found in Trash.', config('textdomain')),
-        ],
-    ]);
+
+
+
 }
-add_action('init', 'Arinks\Theme\App\Structure\register_book_post_type'); */
+add_action('init', 'Arinks\Theme\App\Structure\register_teacher_post_type');*/
+
+
+
